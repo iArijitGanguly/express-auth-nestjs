a7d898a91c52db86dc895043c4d38696b0d097d2
Arijit Ganguly <aganguly158@gmail.com>
2026-08-26 18:21:26 +0530

    #feat: Added health route

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
@@ -7,36 +8,39 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    eslintPluginPrettierRecommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+            sourceType: 'commonjs',
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
     },
-  },
-  {
-    plugins: {
-      'simple-import-sort': simpleImportSort,
+    {
+        plugins: {
+            'simple-import-sort': simpleImportSort,
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-floating-promises': 'off',
+            '@typescript-eslint/no-unsafe-argument': 'warn',
+            semi: ['error', 'always'],
+            // indent: ['error', 4],
+            'simple-import-sort/exports': 'error',
+            'simple-import-sort/imports': 'error',
+            'prettier/prettier': ['error', { endOfLine: 'auto', tabWidth: 4 }],
+        },
     },
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      'simple-import-sort/exports': 'error',
-      'simple-import-sort/imports': 'error',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+    {
+        files: ['eslint.config.mjs'],
+        ...tseslint.configs.disableTypeChecked,
     },
-  },
 );
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
 
 @Module({
-    imports: [],
+    imports: [ConfigModule.forRoot(), HealthModule],
     controllers: [AppController],
     providers: [AppService],
 })
